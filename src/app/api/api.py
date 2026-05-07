0efb74e8c7c17a54762dc86b66c0e27f9d9bb33e
Cao Chinh Bùi <buicaochinh0811@gmail.com>
2026-05-07 16:08:51 +0700

    fix: update src/app/api/api.py

--- src/app/api/api.py
+++ src/app/api/api.py
@@ -4,6 +4,7 @@ from src.app.api.endpoints import integrations
 from src.app.api.endpoints import integrations_telegram
 from src.app.api.endpoints import ingestion
 from src.app.api.endpoints import notion
+from src.app.api.endpoints import games
 
 api_router = APIRouter()
 
@@ -15,3 +16,4 @@ api_router.include_router(integrations.router, prefix="/integrations", tags=["In
 api_router.include_router(integrations_telegram.router, prefix="/integrations", tags=["Integrations"])
 api_router.include_router(ingestion.router, prefix="/ingestion", tags=["Ingestion"])
 api_router.include_router(notion.router, prefix="/notion", tags=["Notion"])
+api_router.include_router(games.router, prefix="/games", tags=["Games"])
